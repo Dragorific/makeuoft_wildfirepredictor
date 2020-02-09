cd3384292bfcea5f1de9a6f254ff9849ad9dc398
SkylarSL <Skylar.S.Liang2728@gmail.com>
2020-02-08 23:57:45 -0500

    made some WILD changes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,6 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <router-link to="/helloworld">DontClickMe</router-link>
     </div>
     <router-view/>
   </div>
--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="hello">
+    <h1>Dont delete the shit below for learning purposes</h1>
     <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
+    <h1>WHAT UP BITCH</h1>
     <h3>Installed CLI Plugins</h3>
     <ul>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
@@ -28,7 +30,10 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+
+    <p>hi guys</p>
   </div>
+  
 </template>
 
 <script>
