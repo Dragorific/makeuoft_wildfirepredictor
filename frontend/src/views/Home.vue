28848f63590fe9b11bf7b38cfa34669f78f85eba
fahmed8383 <fahmed4030@gmail.com>
2020-02-15 14:50:26 -0500

    carousel added

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -10,23 +10,22 @@
         </div>
       </div>
     </div>
-    <div class="project-content">
-      <div class="intro">
-        <div class="container-fluid">
-          <div class="columns">
-            <div class="column">
-              <div class="intro-img"><p>image of something?</p></div>
-            </div>
-            <div class="column">
-              <div class="intro-desc"><p>description of shit</p></div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="columns">
-        <div class="col-12">
-          <div class="info"><p>idk what goes here</p></div>
-        </div>
+    <div class="body">
+      <div class="carousel">
+        <b-carousel
+        :autoplay=false>
+          <b-carousel-item v-for="(carousel, i) in carousels" :key="i">
+              <section class="carouselSlides">
+                  <div class="hero-body has-text-centered">
+                      <h1>{{carousel.heading}}</h1>
+                      <hr>
+                      <div class="carouselText">
+                        <p>{{carousel.text}}</p>
+                      </div>
+                  </div>
+              </section>
+          </b-carousel-item>           
+        </b-carousel>
       </div>
     </div>
   </div>
@@ -38,7 +37,17 @@ import navbar from '@/components/navbar.vue'
 export default {
   components: {
     navbar
-  }
+  },
+  data(){
+        return {
+            carousels: [
+                { heading: 'Scope and Problem', text: "In the first stage of this project, the plan is to develop a basic infrastructure using a machine learning model in Python using Tensorflow and a Linear Regression training model, thoroughly trained to be able to process live, analog data, or data accessed from the web through a service such as “TheWeatherNetwork”, into an accurate percentage representation of the risk of a wildfire. The machine learning model will be able to process all four variables. Furthermore, devices will be assembled and programmed using a variety of ambient sensors that can read the four key variables, for the use of remote deployment in areas where there is no meteorological coverage. A web interface will be built on Vue.js to display the processed information, and this web app will use Golang as the main programming environment for the backend. The web app will be built using Kubernetes, more specifically Docker, to ensure modularity between components and reliable communication between the front-end and back-end. The front end will be able to create graphical representations of the data as well as be able to display the data geographically in a visually aesthetic manner."},
+                { heading: 'Stage 1 | Baseline Detection', text: "In the first stage of this project, the plan is to develop a basic infrastructure using a machine learning model in Python using Tensorflow and a Linear Regression training model, thoroughly trained to be able to process live, analog data, or data accessed from the web through a service such as “TheWeatherNetwork”, into an accurate percentage representation of the risk of a wildfire. The machine learning model will be able to process all four variables. Furthermore, devices will be assembled and programmed using a variety of ambient sensors that can read the four key variables, for the use of remote deployment in areas where there is no meteorological coverage. A web interface will be built on Vue.js to display the processed information, and this web app will use Golang as the main programming environment for the backend. The web app will be built using Kubernetes, more specifically Docker, to ensure modularity between components and reliable communication between the front-end and back-end. The front end will be able to create graphical representations of the data as well as be able to display the data geographically in a visually aesthetic manner."},
+                { heading: 'Stage 2 | Taking Action', text: "The next stage, once the foundation is laid, is to add features which will allow users to interact with the web app for live updates, and create a proof-of-concept autonomous solution that will serve as the baseline for developing more effective means of taking autonomous action towards wildfires. The features added in the next stage will take the appearance of email or cell subscriptions that will alert the subscribed user of any dangerous changes in the live data. The proof-of-concept autonomous solution will take the appearance of autonomous drones programmed to go to the site of the wildfire. This will serve as a baseline for drones in the future to be able to deploy immediately in swarms and control the spread of wildfire through fire fighting methods currently used by larger cargo planes during larger wildfires."},
+                { heading: 'Structure', text: ""},
+            ]
+        }
+    }
 }
 </script>
 
