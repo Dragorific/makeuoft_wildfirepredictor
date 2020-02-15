0ad031ec7787b4ff219d2573bd65aa487f0a7e4b
SkylarSL <Skylar.S.Liang2728@gmail.com>
2020-02-15 14:49:44 -0500

    leap of faith

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,50 +1,38 @@
 <template>
   <div class="about">
     <navbar/>
-    <div class="about-us-container">
-      <div class="columns">
-        <div class="column is-half">
-          <div class="group-img"></div>
-        </div>
-        <div class="column is-half">
-          <div class="group-desc"></div>
-        </div>
-      </div>
-    </div>
     <div class="members-title">
       <p>Members</p>
     </div>
-    <div class="members-container">
-      <div class="columns">
-        <div class="column is-half">
-          <div class="group-member-container">
-            <div class="member-title"><p>Faiq Ahmed</p></div>
-            <div class="group-member-image-container"><img class="member-img-display" src="../assets/images/Faiq.jpg" width="60%"/></div>
-            <div class="group-member-desc-container"><p>I'm faiq and I did nothing in this project</p></div>
-          </div>
-        </div>
-        <div class="column is-half">
-          <div class="group-member-container">
-            <div class="member-title"><p>Umar Khan</p></div>
-            <div class="group-member-image-container"><img class="member-img-display" src="../assets/images/Umar.jpg" width="60%"/></div>
-            <div class="group-member-desc-container"><p>I'm umar and I did nothing in this project</p></div>
-          </div>
+    <div class="columns">
+      <div class="column is-half">
+        <div class="group-member-container">
+          <div class="member-title"><p>Faiq Ahmed</p></div>
+          <div class="group-member-image-container"><img class="member-img-display" src="../assets/images/Faiq.jpg" width="60%"/></div>
+          <div class="group-member-desc-container"><p>I'm faiq and I did nothing in this project</p></div>
         </div>
       </div>
-      <div class="columns">
-        <div class="column is-half">
-          <div class="group-member-container">
-            <div class="member-title"><p>Skylar Liang</p></div>
-            <div class="group-member-image-container"><img class="member-img-display" src="../assets/images/Skylar.png" width="60%"/></div>
-            <div class="group-member-desc-container"><p>I'm skylar and I did nothing in this project</p></div>
-          </div>
+      <div class="column is-half">
+        <div class="group-member-container">
+          <div class="member-title"><p>Umar Khan</p></div>
+          <div class="group-member-image-container"><img class="member-img-display" src="../assets/images/Umar.jpg" width="60%"/></div>
+          <div class="group-member-desc-container"><p>I'm umar and I did nothing in this project</p></div>
         </div>
-        <div class="column is-half">
-          <div class="group-member-container">
-            <div class="member-title"><p>Veerash Palanichamy</p></div>
-            <div class="group-member-image-container"><img class="member-img-display" src="../assets/images/Veerash.jpg" width="60%"/></div>
-            <div class="group-member-desc-container"><p>I'm veerash and I did nothing in this project</p></div>
-          </div>
+      </div>
+    </div>
+    <div class="columns">
+      <div class="column is-half">
+        <div class="group-member-container">
+          <div class="member-title"><p>Skylar Liang</p></div>
+          <div class="group-member-image-container"><img class="member-img-display" src="../assets/images/Skylar.png" width="60%"/></div>
+          <div class="group-member-desc-container"><p>I'm skylar and I did nothing in this project</p></div>
+        </div>
+      </div>
+      <div class="column is-half">
+        <div class="group-member-container">
+          <div class="member-title"><p>Veerash Palanichamy</p></div>
+          <div class="group-member-image-container"><img class="member-img-display" src="../assets/images/Veerash.jpg" width="60%"/></div>
+          <div class="group-member-desc-container"><p>I'm veerash and I did nothing in this project</p></div>
         </div>
       </div>
     </div>
