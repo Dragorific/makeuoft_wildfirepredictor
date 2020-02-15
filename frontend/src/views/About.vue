d99dfa500db159cce15cf0d82ad82427aabb15ef
SkylarSL <Skylar.S.Liang2728@gmail.com>
2020-02-15 13:49:51 -0500

    adding images to mmbers page, complete except for descriptions

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -8,14 +8,14 @@
       <div class="column is-half">
         <div class="group-member-container">
           <div class="member-title"><p>Faiq Ahmed</p></div>
-          <div class="group-member-image-container"><img src="" alt="FAIQ"/></div>
+          <div class="group-member-image-container"><img class="member-img-display" src="../assets/images/Faiq.jpg" width="60%"/></div>
           <div class="group-member-desc-container"><p>I'm faiq and I did nothing in this project</p></div>
         </div>
       </div>
       <div class="column is-half">
         <div class="group-member-container">
           <div class="member-title"><p>Umar Khan</p></div>
-          <div class="group-member-image-container"><img src="" alt="UMAR"/></div>
+          <div class="group-member-image-container"><img class="member-img-display" src="../assets/images/Umar.jpg" width="60%"/></div>
           <div class="group-member-desc-container"><p>I'm umar and I did nothing in this project</p></div>
         </div>
       </div>
@@ -24,14 +24,14 @@
       <div class="column is-half">
         <div class="group-member-container">
           <div class="member-title"><p>Skylar Liang</p></div>
-          <div class="group-member-image-container"><img src="" alt="SKYLAR"/></div>
+          <div class="group-member-image-container"><img class="member-img-display" src="../assets/images/Skylar.png" width="60%"/></div>
           <div class="group-member-desc-container"><p>I'm skylar and I did nothing in this project</p></div>
         </div>
       </div>
       <div class="column is-half">
         <div class="group-member-container">
           <div class="member-title"><p>Veerash Palanichamy</p></div>
-          <div class="group-member-image-container"><img src="" alt="VEERASH"/></div>
+          <div class="group-member-image-container"><img class="member-img-display" src="../assets/images/Veerash.jpg" width="60%"/></div>
           <div class="group-member-desc-container"><p>I'm veerash and I did nothing in this project</p></div>
         </div>
       </div>
