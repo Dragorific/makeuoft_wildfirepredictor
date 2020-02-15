72502edcb270ea8bd297f428c435590ae6b379a3
SkylarSL <Skylar.S.Liang2728@gmail.com>
2020-02-15 17:31:36 -0500

    added google maps

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,9 +3,38 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
+import * as VueGoogleMaps from 'vue2-google-maps'
+import {GmapMarker} from 'vue2-google-maps/src/components/marker'
 
+Vue.component('GmapMarker', GmapMarker)
 Vue.use(VueRouter)
 Vue.use(Buefy)
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: "AIzaSyDpXEHLIiZEyRvzVI3fHWKmFYkiTKX83eI",
+    libraries: 'places', // This is required if you use the Autocomplete plugin
+    // OR: libraries: 'places,drawing'
+    // OR: libraries: 'places,drawing,visualization'
+    // (as you require)
+
+    //// If you want to set the version, you can do so:
+    // v: '3.26',
+  },
+
+  //// If you intend to programmatically custom event listener code
+  //// (e.g. `this.$refs.gmap.$on('zoom_changed', someFunc)`)
+  //// instead of going through Vue templates (e.g. `<GmapMap @zoom_changed="someFunc">`)
+  //// you might need to turn this on.
+  // autobindAllEvents: false,
+
+  //// If you want to manually install components, e.g.
+  //// import {GmapMarker} from 'vue2-google-maps/src/components/marker'
+  //// Vue.component('GmapMarker', GmapMarker)
+  //// then set installComponents to 'false'.
+  //// If you want to automatically install all the components this property must be set to 'true':
+  installComponents: true
+})
+
 const routes = [
   {
     path: '/',
