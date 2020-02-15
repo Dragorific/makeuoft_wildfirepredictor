44bc22bd415b013869e4a346d2abc432dab9be9f
SkylarSL <Skylar.S.Liang2728@gmail.com>
2020-02-15 15:31:16 -0500

    reverted my updates

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,52 +1,3 @@
-<<<<<<< HEAD
-<template>
-  <div class="home">
-    <navbar/>
-    <div class="front-page">
-      <div class="paralax">
-        <div class = "overlay">
-        </div>
-        <div class="project-title">
-          <h1>Wildfire Predictor Model 2.0</h1>
-        </div>
-      </div>
-    </div>
-    <div class="project-content">
-      <div class="intro">
-        <div class="container-fluid">
-          <div class="columns">
-            <div class="column">
-              <div class="intro-img"><p>image of something?</p></div>
-            </div>
-            <div class="column">
-              <div class="intro-desc"><p>description of shit</p></div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="columns">
-        <div class="col-12">
-          <div class="info"><p>idk what goes here</p></div>
-        </div>
-      </div>
-    </div>
-  </div>
-</template>
-
-<script>
-import navbar from '@/components/navbar.vue'
-
-export default {
-  components: {
-    navbar
-  }
-}
-</script>
-
-<style scoped>
-  @import '../assets/css/HomePage.scss'
-</style>
-=======
 <template>
   <div class="home">
     <navbar/>
@@ -103,4 +54,3 @@ export default {
 <style scoped>
   @import '../assets/css/HomePage.scss'
 </style>
->>>>>>> 28848f63590fe9b11bf7b38cfa34669f78f85eba
