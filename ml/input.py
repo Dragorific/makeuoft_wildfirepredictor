87d351ae3aaea5c862ac9b412b062c9c95d287a6
Veerash Palanichamy <veerashp05@gmail.com>
2020-02-16 09:10:08 -0500

    updated ml prediction

--- ml/input.py
+++ ml/input.py
@@ -4,21 +4,26 @@ import pandas as pd
 import seaborn
 import matplotlib.pyplot as plt
 
-data = []
-csvfile = open("forestfires.csv", newline='')
-outfile = open('modifiedData.csv', 'w', newline='')
-reader = csv.reader(csvfile)
-writer = csv.writer(outfile)
-print(reader.__next__())
-for line in reader:
-    if (float(line[-1]) > 0):
-        temp  = line[5:-1]
-        temp.append("1")
-        writer.writerow(temp)
-        data.append(line[5:-1].append('1'))
-    else:
-        writer.writerow(line[5:])
-        data.append(line[5:])
+months = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"]
 
-csvfile.close()
-outfile.close()
+def numMonth(a):
+    return [months.index(a)]
+
+if __name__ == "__main__":
+
+    data = []
+    csvfile = open("forestfires.csv", newline='')
+    outfile = open('modifiedData2.csv', 'w', newline='')
+    reader = csv.reader(csvfile)
+    writer = csv.writer(outfile)
+    print(reader.__next__())
+    for line in reader:
+        if (float(line[-1]) > 0):
+            temp  = line[0:2]+numMonth(line[2])+line[4:-1]
+            temp.append("1")
+            writer.writerow(temp)
+        else:
+            writer.writerow(line[0:2]+numMonth(line[2])+line[4:])
+
+    csvfile.close()
+    outfile.close()
